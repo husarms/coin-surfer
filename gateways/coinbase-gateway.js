c8418a36e88f65d57ed438cd3b3c638a6337f535
husarms <husarms@gmail.com>
2021-05-10 16:32:20 -0400

    Working commit

--- gateways/coinbase-gateway.js
+++ gateways/coinbase-gateway.js
@@ -1,3 +1,129 @@
-const CoinbasePro = require('coinbase-pro');
-const publicClient = new CoinbasePro.PublicClient();
+const CoinbasePro = require("coinbase-pro");
+const Secrets = require("./secrets");
 
+const productionURI = "https://api.pro.coinbase.com";
+const sandboxURI = "https://api-public.sandbox.pro.coinbase.com";
+
+const authenticatedProductionClient = new CoinbasePro.AuthenticatedClient(
+    Secrets.CoinbaseProductionConfiguration.key,
+    Secrets.CoinbaseProductionConfiguration.secret,
+    Secrets.CoinbaseProductionConfiguration.passphrase,
+    productionURI
+);
+
+const authenticatedSandboxClient = new CoinbasePro.AuthenticatedClient(
+    Secrets.CoinbaseSandboxConfiguration.key,
+    Secrets.CoinbaseSandboxConfiguration.secret,
+    Secrets.CoinbaseSandboxConfiguration.passphrase,
+    sandboxURI
+);
+
+module.exports = {
+    getProductTicker: async function (product_id) {
+        return authenticatedProductionClient
+            .getProductTicker(product_id)
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    getProduct24HrStats: async function (product) {
+        return authenticatedProductionClient
+            .getProduct24HrStats(product)
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    getAccounts: async function () {
+        return authenticatedProductionClient
+            .getAccounts()
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    getCoinbaseAccounts: async function () {
+        return authenticatedProductionClient
+            .getCoinbaseAccounts()
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    getAccount: async function (account_id) {
+        return authenticatedProductionClient
+            .getAccount(account_id)
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    limitBuy: async function (price, size, product_id) {
+        return authenticatedProductionClient
+            .buy({
+                price,
+                size,
+                product_id,
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    marketBuy: async function (funds, size, product_id) {
+        return authenticatedProductionClient
+            .buy({
+                type: 'market',
+                funds,
+                size,
+                product_id,
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    limitSell: async function (price, size, product_id) {
+        return authenticatedProductionClient
+            .sell({
+                price,
+                size,
+                product_id,
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+    marketSell: async function (size, product_id) {
+        return authenticatedProductionClient
+            .sell({
+                type: 'market',
+                size,
+                product_id,
+            })
+            .then((data) => {
+                return data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    },
+};
---
+++ index.js
@@ -0,0 +1,129 @@
+const CoinbaseGateway = require("./gateways/coinbase-gateway");
+
+const Bitcoin = "BTC";
+const BitcoinCash = "BCH";
+const Etherium = "ETH";
+const USDollar = "USD";
+
+const getAccountBalance = async (currency) => {
+    var accounts = await CoinbaseGateway.getAccounts();
+    var account = accounts.find((a) => a.currency === currency);
+    return roundDownToTwoDecimals(parseFloat(account.balance));
+};
+
+const get24HrAveragePrice = async (cryptoCurrency) => {
+    var product24HrStats = await CoinbaseGateway.getProduct24HrStats(
+        cryptoCurrency
+    );
+    const high = parseFloat(product24HrStats.high);
+    const low = parseFloat(product24HrStats.low);
+    let average = high - (high - low) / 2;
+    return average;
+};
+
+const getBuyThreshold = (averagePrice, thresholdPercentage) => {
+    return (averagePrice - averagePrice * (thresholdPercentage / 100)).toFixed(
+        2
+    );
+};
+
+const getSellThreshold = (averagePrice, thresholdPercentage) => {
+    return (averagePrice + averagePrice * (thresholdPercentage / 100)).toFixed(
+        2
+    );
+};
+
+const getBuySize = (fiatBalance, productPrice) => {
+    return Math.floor((fiatBalance / productPrice) * 10000) / 10000;
+};
+
+const roundDownToTwoDecimals = (number) => {
+    return Math.floor(number * 100) / 100;
+};
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString();
+};
+
+(async () => {
+    const fiatCurrency = USDollar;
+    const cryptoCurrency = BitcoinCash;
+    const productId = `${cryptoCurrency}-${fiatCurrency}`;
+    const buyThresholdPercentage = 5;
+    const sellThresholdPercentage = 5;
+    const fiatBalance = await getAccountBalance(fiatCurrency);
+    const cryptoBalance = await getAccountBalance(cryptoCurrency);
+    let lookingToSell = fiatBalance < 10;
+    console.log(`Current fiat balance = $${fiatBalance} (${fiatCurrency})`);
+    console.log(
+        `Looking to ${
+            lookingToSell ? "sell" : "buy"
+        } ${cryptoCurrency} (current balance = ${cryptoBalance})...`
+    );
+
+    const averagePriceInLast24Hrs = await get24HrAveragePrice(productId);
+    console.log(
+        `Average price for '${productId}' in last 24 hours = $${averagePriceInLast24Hrs}...`
+    );
+
+    const interval = setInterval(async function () {
+        const averagePrice = await get24HrAveragePrice(productId);
+        const buyThreshold = getBuyThreshold(
+            averagePrice,
+            buyThresholdPercentage
+        );
+        const sellThreshold = getSellThreshold(
+            averagePrice,
+            sellThresholdPercentage
+        );
+        const ticker = await CoinbaseGateway.getProductTicker(productId);
+        const price = parseFloat(ticker.price);
+        console.log(
+            `Current ${productId} price at ${formatDate(
+                ticker.time
+            )} = $${price} (buy @${buyThreshold}, sell @${sellThreshold})`
+        );
+
+        if (lookingToSell) {
+            if (price >= sellThreshold) {
+                console.log(
+                    `Sell threshold hit (${price} >= ${sellThreshold})`
+                );
+                const cryptoBalance = await getAccountBalance(cryptoCurrency);
+                const size = cryptoBalance;
+                console.log(
+                    `Selling ${size} ${cryptoCurrency} at $${price}, ${fiatCurrency} value = $${
+                        size * price
+                    }`
+                );
+                const sellResponse = CoinbaseGateway.marketSell(
+                    cryptoBalance,
+                    productId
+                );
+                console.log(
+                    `Sell complete. Response = ${JSON.stringify(sellResponse)}`
+                );
+                lookingToSell = false;
+            }
+        } else {
+            if (price <= buyThreshold) {
+                console.log(`Buy threshold hit (${price} <= ${buyThreshold})`);
+                const fiatBalance = await getAccountBalance(fiatCurrency);
+                const size = getBuySize(fiatBalance, price);
+                console.log(
+                    `Buying ${size} ${cryptoCurrency} at $${price}, ${fiatCurrency} value = $${
+                        size * price
+                    }`
+                );
+                const buyResponse = await CoinbaseGateway.marketBuy(
+                    fiatBalance,
+                    size,
+                    productId
+                );
+                console.log(`Buy complete. Order Id = ${buyResponse.id}`);
+                lookingToSell = true;
+            }
+        }
+    }, 10000);
+})();
