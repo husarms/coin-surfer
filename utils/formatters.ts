07358169934d5b8a4fc3a53d41e8955a6da327f3
husarms <husarms@gmail.com>
2021-07-27 21:29:43 -0400

    Add missing formatter

--- utils/formatters.ts
+++ utils/formatters.ts
@@ -15,6 +15,27 @@ export function getDateMMddyyyyHHmmss() {
     return now.toLocaleString().replace(",", "");
 }
 
+export function formatDateyyyyMMddHHmmss(dateString: string) {
+    const date = new Date(dateString);
+    let d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+    let hour = "" + d.getHours();
+    let min = "" + d.getMinutes();
+    let sec = "" + d.getSeconds();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    if (hour.length < 2) hour = "0" + hour;
+    if (min.length < 2) min = "0" + min;
+    if (sec.length < 2) sec = "0" + sec;
+
+    var result = [year, month, day].join("");
+
+    return result + "-" + hour + min + sec;
+}
+
 export function getDateyyyyMMddHHmmss() {
     const now = new Date();
     let d = new Date(now),
