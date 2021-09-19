0c60f9d21c4db61df5581f43a7be899683af758e
husarms <husarms@gmail.com>
2021-09-19 15:20:22 -0400

    Update visualization image, data points

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,9 +8,9 @@ import useWebSocket from "react-use-websocket";
 const priceColor = "#5E4FA2";
 const averageColor = "#A0D3FF";
 const thresholdColor = "#A8DF53";
-
+let updateCount = 0;
 function App() {
-    const maxDataPoints = 100000;
+    const maxDataPoints = 50000;
     const { lastMessage } = useWebSocket("ws://localhost:8080/ws", {
         onMessage: (messageEvent: WebSocketEventMap["message"]) => {
             handleMessage(messageEvent);
@@ -20,35 +20,38 @@ function App() {
     const [priceData, setPriceData] = useState([]);
     const [averageData, setAverageData] = useState([]);
     const [thresholdData, setThresholdData] = useState([]);
-
     const handleMessage = (message) => {
         let count = messageCount;
         setMessageCount(count + 1);
-        const timestamp = new Date();
-        const { price, low_24h, high_24h } = JSON.parse(message.data);
-        const average =
-            (parseFloat(high_24h) - parseFloat(low_24h)) / 2 +
-            parseFloat(low_24h);
+        updateCount++;
+        if (updateCount >= 10) {
+            updateCount = 0;
+            const timestamp = new Date();
+            const { price, low_24h, high_24h } = JSON.parse(message.data);
+            const average =
+                (parseFloat(high_24h) - parseFloat(low_24h)) / 2 +
+                parseFloat(low_24h);
 
-        const threshold =
-            state.action === Actions.Buy
-                ? state.buyThreshold
-                : state.sellThreshold;
+            const threshold =
+                state.action === Actions.Buy
+                    ? state.buyThreshold
+                    : state.sellThreshold;
 
-        let data = priceData;
-        (data as any).push({ value: parseFloat(price), date: timestamp });
-        if (data.length >= maxDataPoints) data.shift();
-        setPriceData(data);
+            let data = priceData;
+            (data as any).push({ value: parseFloat(price), date: timestamp });
+            if (data.length >= maxDataPoints) data.shift();
+            setPriceData(data);
 
-        data = averageData;
-        (data as any).push({ value: average, date: timestamp });
-        if (data.length >= maxDataPoints) data.shift();
-        setAverageData(data);
+            data = averageData;
+            (data as any).push({ value: average, date: timestamp });
+            if (data.length >= maxDataPoints) data.shift();
+            setAverageData(data);
 
-        data = thresholdData;
-        (data as any).push({ value: threshold, date: timestamp });
-        if (data.length >= maxDataPoints) data.shift();
-        setThresholdData(data);
+            data = thresholdData;
+            (data as any).push({ value: threshold, date: timestamp });
+            if (data.length >= maxDataPoints) data.shift();
+            setThresholdData(data);
+        }
     };
     const formatNumber = (number: number): string => {
         return (Math.round(number * 100) / 100).toFixed(2);
