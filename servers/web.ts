ee11ec3341993930c4fd7c6473b831bb78c45276
Matt Husar <husarms@gmail.com>
2022-01-01 15:03:24 -0500

    Remote start the surfers

--- servers/web.ts
+++ servers/web.ts
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-export const startWebServer = (port?: string | number) => {
+let backgroundProcessRunning = false;
+
+export const startWebServer = (backgroundProcess: () => void, port?: string | number) => {
     const app = express();
     port = port ? port : process.env.PORT;
     app.use(express.static('web/dist'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/web/dist/index.html'));
+    app.get('/hello', function (req, res) {
+        console.log("Received request to /hello");
+        let didWeJustTurnOnBackgroundProcess = false;
+        if (!backgroundProcessRunning) {
+            backgroundProcess();
+            backgroundProcessRunning = true;
+            didWeJustTurnOnBackgroundProcess = true;
+        }
+        res.send(`Did we just turn on background process? ${didWeJustTurnOnBackgroundProcess}`);
+    });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../web/dist/index.html'));
     });
     app.listen(port, () => {
         console.log(`Web server started at http://${process.env.BASE_ADDRESS}:${port}`);
