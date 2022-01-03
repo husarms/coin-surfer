962483a108bcab9818e07f77aceafc661d937223
Matt Husar <husarms@gmail.com>
2022-01-03 16:26:03 -0500

    Add icons, update buttons

--- web/src/pages/AboutPage.tsx
+++ web/src/pages/AboutPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, LayoutSingleColumn } from "../components";
+import { Button, Card, LayoutSingleColumn } from "../components";
+import { FaGithub } from 'react-icons/fa';
 
 
 function AboutPage(): JSX.Element {
@@ -7,12 +8,14 @@ function AboutPage(): JSX.Element {
         <LayoutSingleColumn>
             <h1 className="display-2 p-t-2xl p-b-m">Coin Surfe<span className="vim-caret">r</span></h1>
             <h1 className="display-5 p-b-2xl text-mono color-green">Let's go surfing.</h1>
+            <Button size='large' shadow={true} icon={true} className='m-b-2xl' onClick={() => window.open('https://github.com/husarms/coin-surfer#readme', '_blank')}>
+                <FaGithub />Check it out on GitHub
+            </Button>
             <Card>
-                <div>
+                <div className="body-2">
                     <p className="p-t-m p-b-l">Coin Surfer is an open-source project for automated cryptocurrency trading.</p>
                     <p className="p-b-l">This site is a live demo version of the visualization app using the example "AI threshold" surfer included in the project.</p>
-                    <p className="p-b-l">The tab(s) above correspond with the product(s) that are currently being traded.</p>
-                    <p className="p-b-s">To learn more, check out the project on <a href="https://github.com/husarms/coin-surfer#readme" target="_blank">GitHub</a>.</p>
+                    <p className="p-b-m">The tab(s) above correspond with the product(s) that are currently being traded.</p>
                 </div>
             </Card>
             <div className="p-t-3xl">
