bf3db6911cb14c430c3d2105b0c406f82bf7b5fa
Matt Husar <husarms@gmail.com>
2022-03-07 19:18:44 -0500

    Working commit

--- web/src/pages/AboutPage.tsx
+++ web/src/pages/AboutPage.tsx
@@ -1,18 +1,31 @@
 import React from "react";
-import { Button, Card, IconContainer, LayoutSingleColumn } from "../components";
+import { BoundedWaveLoader, Button, Card, IconContainer, LayoutSingleColumn } from "../components";
 import { FaGithub } from 'react-icons/fa';
 
+export interface AboutPageProps {
+    isLoading: boolean;
+}
 
-function AboutPage(): JSX.Element {
+function AboutPage({ isLoading }: AboutPageProps): JSX.Element {
     return (
         <LayoutSingleColumn>
             <h1 className="display-2 p-t-2xl p-b-m">Coin Surfe<span className="vim-caret">r</span></h1>
-            <h1 className="display-5 p-b-2xl text-mono color-green">Let's go surfing.</h1>
+            {
+                isLoading ? (
+                    <div className="p-b-xl">
+                        <BoundedWaveLoader />
+                    </div>
+                ) : (
+                    <h1 className="display-5 p-b-2xl text-mono color-green">Let's go surfing.</h1>
+                )
+            }
             <Card>
                 <div className="body-2">
                     <p className="p-t-m p-b-l">Coin Surfer is an open-source project for automated cryptocurrency trading.</p>
                     <p className="p-b-l">This site is a live demo version of the visualization app using the example "AI threshold" surfer included in the project.</p>
-                    <p className="p-b-m">The tab(s) above correspond with the product(s) that are currently being traded.</p>
+                    {
+                        !isLoading && <p className="p-b-m">The tab(s) above correspond with the product(s) that are currently being traded.</p>
+                    }
                 </div>
             </Card>
             <Button size='large' shadow={true} className='m-t-3xl' onClick={() => window.open('https://github.com/husarms/coin-surfer#readme', '_blank')}>
