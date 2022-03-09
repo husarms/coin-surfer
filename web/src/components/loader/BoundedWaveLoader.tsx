a1c44c274ffc43c0cd68345c54d67258986d3d79
Matt Husar <husarms@gmail.com>
2022-03-08 23:42:20 -0500

    Update BoundedWaveLoader.tsx

--- web/src/components/loader/BoundedWaveLoader.tsx
+++ web/src/components/loader/BoundedWaveLoader.tsx
@@ -5,15 +5,15 @@ function Wave(): JSX.Element {
     return (
         <div>
             <svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
                 <defs>
                     <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                 </defs>
                 <g className={styles.parallax}>
-                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255, 0.7" />
-                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255, 0.5)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255, 0.3)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(0,97,255,0.4)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(0,97,255,0.6)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(96,239,255,0.4)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(96,239,255,0.6)" />
                 </g>
             </svg>
         </div>
