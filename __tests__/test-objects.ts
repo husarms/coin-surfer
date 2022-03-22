7c4e2dd303227d2d9de9bc8325aaed91ed2f3a84
Matt Husar <husarms@gmail.com>
2022-03-22 19:15:21 -0400

    Refactor for testing

--- __tests__/test-objects.ts
+++ __tests__/test-objects.ts
@@ -1,5 +1,6 @@
 import SurfParameters from "../interfaces/surf-parameters";
 import SurfState from "../interfaces/surf-state";
+import TrendAnalysis from "../interfaces/trend-analysis";
 import { Actions, Products } from "../utils/enums";
 import * as Formatters from "../utils/formatters";
 
@@ -11,6 +12,25 @@ const surfParameters : SurfParameters = {
     budget: 50000,
     notificationsEnabled: true,
     webSocketFeedEnabled: true,
+    isLocal: true,
+}
+
+const trendAnalysis: TrendAnalysis = {
+    sevenDayAverage: 45000,
+    sevenDayHighPrice: 47000,
+    sevenDayLowPrice: 43000,
+    thirtyDayAverage: 45000,
+    thirtyDayHighPrice: 47000,
+    thirtyDayLowPrice: 43000,
+    sixtyDayAverage: 45000,
+    sixtyDayHighPrice: 47000,
+    sixtyDayLowPrice: 43000,
+    ninetyDayAverage: 45000,
+    ninetyDayHighPrice: 47000,
+    ninetyDayLowPrice: 43000,
+    oneTwentyDayAverage: 45000,
+    oneTwentyDayHighPrice: 47000,
+    oneTwentyDayLowPrice: 43000,
 }
 
 const surfState : SurfState = {
@@ -20,12 +40,17 @@ const surfState : SurfState = {
     price: 45000,
     averagePrice: 46000,
     buyThreshold: 43000,
+    buyThresholdPercentage: 5,
     sellThreshold: 48000,
+    sellThresholdPercentage: 5,
     lastBuyPrice: 42000,
     lastBuyDate: Formatters.addDays(new Date(), -4),
     lastSellDate: Formatters.addDays(new Date(), -1),
     fiatBalance: 25000,
-    cryptoBalance: 0
+    cryptoBalance: 0,
+    trendAnalysis,
+    statusMessage: '',
+    timestamp: new Date()
 }
 
-export default { surfParameters, surfState }
+export default { surfParameters, surfState, trendAnalysis }
--- surfers/ai-threshold.ts
+++ surfers/ai-threshold.ts
@@ -13,31 +13,40 @@ import { Actions } from "../utils/enums";
 import SurfParameters from "../interfaces/surf-parameters";
 import SurfState from "../interfaces/surf-state";
 
-export async function surf(parameters: SurfParameters): Promise<NodeJS.Timer> {
+export async function startSurfing(parameters: SurfParameters): Promise<NodeJS.Timer> {
+    let state = await initializeState(parameters);
+    const logger = new Logger(state.productId);
+    console.log(`Let's go AI threshold surfing with ${state.productId}...`);
+    return setInterval(async function () {
+        state = await surf(state, logger);
+    }, 10000);
+}
+
+export async function initializeState(parameters: SurfParameters): Promise<SurfState> {
     let state = {} as SurfState;
     state.parameters = parameters;
     state.productId = `${state.parameters.cryptoCurrency}-${state.parameters.fiatCurrency}`;
-    const logger = new Logger(state.productId);
     state = await updateBalances(state);
     state.action = state.cryptoBalance > 0.1 ? Actions.Sell : Actions.Buy;
+    return state;
+}
 
-    console.log(`Let's go AI threshold surfing with ${state.productId}...`);
-    return setInterval(async function () {
-        state = await updateBalances(state);
-        state = await updateFills(state);
-        state = await updatePrices(state);
-        state = await updateTrendAnalysis(state);
-        state = await updateThresholdsWithAI(state);
-        state = updateStatus(state, logger);
-        const { action, price, buyThreshold, sellThreshold } = state;
-        if (action === Actions.Sell) {
-            if (price >= sellThreshold) {
-                state = await handleSell(state);
-            }
-        } else {
-            if (price <= buyThreshold) {
-                state = await handleBuy(state);
-            }
+export async function surf(state: SurfState, logger: Logger): Promise<SurfState> {
+    state = await updateBalances(state);
+    state = await updateFills(state);
+    state = await updatePrices(state);
+    state = await updateTrendAnalysis(state);
+    state = await updateThresholdsWithAI(state);
+    state = updateStatus(state, logger);
+    const { action, price, buyThreshold, sellThreshold } = state;
+    if (action === Actions.Sell) {
+        if (price >= sellThreshold) {
+            state = await handleSell(state);
         }
-    }, 10000);
+    } else {
+        if (price <= buyThreshold) {
+            state = await handleBuy(state);
+        }
+    }
+    return state;
 }
